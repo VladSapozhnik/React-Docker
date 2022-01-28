e53d7aa88994cef807f3e0c0b80acff6acae8881
VladSapozhnik <vladbars2@gmail.com>
2022-01-28 16:56:24 +0200

    save

--- frontend/src/components/AnyReactComponent.js
+++ frontend/src/components/AnyReactComponent.js
@@ -2,9 +2,23 @@ import React from "react";
 
 
 function AnyReactComponent (prop) {
+
+    // const [open, setOpen] = useState(false)
+    const styles = {
+        img: {
+          width: '25px',
+          height: '25px'
+        },
+        text: {
+            fontWeight: '600',
+        }
+    }
+
+
     return (
         <div>
-            {prop.text}
+            <img style={styles.img} src="images/marker.svg" alt=""/>
+            {/*<p style={styles.text}>{prop.text}</p>*/}
         </div>
     )
 }
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -3,26 +3,41 @@ import GoogleMapReact from 'google-map-react';
 import AnyReactComponent from "./AnyReactComponent";
 
 function Map () {
-    const markers = [
-        {
-            lat: 10.99835602,
-            lng: 77.01502627,
-            text: 'Content 1'
-        },
-        {
-            lat: 11.99835602,
-            lng: 77.01502627,
-            text: 'Content 2'
-        },
-        {
-            lat: 12.99835602,
-            lng: 77.01502627,
-            text: 'Content 3'
-        }
-    ]
+    // const markers = [
+    //     {
+    //         lat: 10.99835602,
+    //         lng: 77.01502627,
+    //         text: 'Content 1'
+    //     },
+    //     {
+    //         lat: 11.99835602,
+    //         lng: 77.01502627,
+    //         text: 'Content 2'
+    //     },
+    //     {
+    //         lat: 12.99835602,
+    //         lng: 77.01502627,
+    //         text: 'Content 3'
+    //     }
+    // ]
+
+    // const GoogleMaps = ({ latitude, longitude }) => {
+    //     const renderMarkers = (map, maps) => {
+    //         let marker = new maps.Marker({
+    //             position: { lat: latitude, lng: longitude },
+    //             map,
+    //             title: 'Hello World!'
+    //         });
+    //         return console.log(marker, 'marker');
+    //     }
+    //     return console.log(renderMarkers, 'render markers')
+    // }
+    //
+    // console.log(GoogleMaps)
 
 
     const defaultProps = {
+
         center: {
             lat: 10.99835602,
             lng: 77.01502627
@@ -32,18 +47,18 @@ function Map () {
     return (
         <div style={{ height: '400px', width: '100%' }}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: "" }}
+                bootstrapURLKeys={{ key: "AIzaSyCpH5wELSQzDiT_Snv_ClnmbaPsUpzhSXk" }}
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
                 // ref={map => map && map.fitBounds(bounds)}
             >
-                {markers.map((item, index) => <AnyReactComponent lat={item.lat} lng={item.lng} text={item.text} key={index}/>)}
+                {/*{markers.map((item, index) => <AnyReactComponent lat={item.lat} lng={item.lng} text={item.text} key={index}/>)}*/}
 
-                {/*<AnyReactComponent*/}
-                {/*    lat={10.99835602}*/}
-                {/*    lng={77.01502627}*/}
-                {/*    text="My Marker"*/}
-                {/*/>*/}
+                <AnyReactComponent
+                    lat={10.99835602}
+                    lng={77.01502627}
+                    text="My Marker"
+                />
             </GoogleMapReact>
         </div>
     )
